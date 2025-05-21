8b2b1615209b1286c66af9437b014ff65460409d
Aftabrahaman <115376925+Aftabrahaman@users.noreply.github.com>
2025-05-21 13:31:21 +0530

    Update padding_m.sv

--- padding_m.sv
+++ padding_m.sv
@@ -25,16 +25,18 @@ input logic [511:0] m,input int unsigned siz,output logic [511:0] pm
 );
     function logic [511:0] pad_message(input logic [511:0] message, input int  unsigned   bit_length);
     logic [511:0] padded_block;
-//    int unsigned zero_pad_length; 
-//    // Calculate how many zeros we need to add after the '1' bit
-//    // We need 64 bits at the end for length, plus at least one '1' bit
-//    zero_pad_length = 512 - (bit_length % 512) - 1 - 64;
-    
+     logic [511:0] reverse;
+        int unsigned half_byte;
+        half_byte=bit_length/4;
+        for (int i=0;i<half_byte;i=i+1)begin
+        reverse[i*4 +:4]=message[(half_byte-1-i)*4 +:4];
+        end 
+        
     // Initialize to 0
     padded_block = '0;
     // First part: the message itself
 //    padded_block[511 -: bit_length] = message;
-   padded_block = message & ((1 << bit_length) - 1);
+   padded_block = reverse & ((1 << bit_length) - 1);
     // Append '1' bit
     padded_block[bit_length] = 1'b1;
     
